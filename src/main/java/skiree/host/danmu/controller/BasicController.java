11db0303544172902363fd7656b2ec7cad3a0f7d
guochongling <2940397985@qq.com>
2024-06-15 14:59:29 +0800

    1. 整合Sa-Token 2. 页面初步整理

--- src/main/java/skiree/host/danmu/controller/BasicController.java
+++ src/main/java/skiree/host/danmu/controller/BasicController.java
@@ -1,12 +1,16 @@
 package skiree.host.danmu.controller;
 
+import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.core.date.DateUtil;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 import org.quartz.impl.matchers.GroupMatcher;
 import org.quartz.impl.triggers.CronTriggerImpl;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -17,9 +21,36 @@ public class BasicController {
 
     private static final SchedulerFactory gSchedulerFactory = new StdSchedulerFactory();
 
+    @Value("${danmu.user}")
+    private String user;
+
+    @Value("${danmu.password}")
+    private String fortPassword;
+
     @GetMapping("/")
     public String index() {
         return "login";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/doLogout")
+    public String doLogout() {
+        StpUtil.logout("Admin");
+        return "redirect:/login";
+    }
+
+    @PostMapping("/doLogin")
+    public String doLogin(Model model, String username, String password) {
+        if (user.equals(username) && fortPassword.equals(password)) {
+            model.addAttribute("loginName", "Admin");
+            StpUtil.login("Admin");
+            return "home";
+        }
+        return "loginFailed";
     }
 
     // http://127.0.0.1:8091/job
