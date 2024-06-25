84174c57627c6280d27c7ea26e6018d0544417b2
guochongling <2940397985@qq.com>
2024-06-25 11:12:22 +0800

    1. 解决重启数据丢失问题

--- src/main/java/skiree/host/danmu/DanmuApplication.java
+++ src/main/java/skiree/host/danmu/DanmuApplication.java
@@ -1,14 +1,15 @@
 package skiree.host.danmu;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import skiree.host.danmu.service.core.Stratege;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.plugin.core.config.EnablePluginRegistries;
 
-@SpringBootApplication
 @MapperScan("skiree.host.danmu.dao")
 @EnablePluginRegistries({Stratege.class})
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class DanmuApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/skiree/host/danmu/util/config/DataSourceConfig.java
@@ -0,0 +1,38 @@
+package skiree.host.danmu.util.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import javax.sql.DataSource;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("classpath:db/danmu.db")
+    private Resource databaseResource;
+
+    @Bean
+    public DataSource getDataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        try {
+            String dbPath = System.getProperty("user.dir") + "/".replace("file:/", "/") + "/danmu.db";
+            Path databaseFile = Paths.get(dbPath);
+            if (!Files.exists(databaseFile)) {
+                Files.copy(databaseResource.getInputStream(), databaseFile, StandardCopyOption.REPLACE_EXISTING);
+            }
+            dataSource.setUrl("jdbc:sqlite:" + dbPath);
+        } catch (Exception e) {
+            throw new RuntimeException("初始化danMu.db时异常", e);
+        }
+        dataSource.setValidationQuery("SELECT 1");
+        return dataSource;
+    }
+
+}
