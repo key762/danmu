d7b641eca2fa9c45fd510d69dd07c4826aea3efd
guochongling <2940397985@qq.com>
2024-07-08 13:39:12 +0800

    1. dns修复

--- src/main/java/skiree/host/danmu/util/start/StartDanMuListener.java
+++ src/main/java/skiree/host/danmu/util/start/StartDanMuListener.java
@@ -1,11 +1,13 @@
 package skiree.host.danmu.util.start;
 
-import io.leopard.javahost.JavaHost;
+import com.alibaba.dcm.DnsCacheManipulator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import skiree.host.danmu.service.base.ConfigService;
+
+import java.net.InetAddress;
 
 @Component
 public class StartDanMuListener implements ApplicationRunner {
@@ -16,7 +18,7 @@ public class StartDanMuListener implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         configService.updateConfig();
-        JavaHost.updateVirtualDns("api.themoviedb.org", "13.32.50.105");
-        JavaHost.printAllVirtualDns();
+        DnsCacheManipulator.setDnsCache("api.themoviedb.org", "13.32.50.105");
+        System.out.println(InetAddress.getByName("api.themoviedb.org").getHostAddress());
     }
 }
